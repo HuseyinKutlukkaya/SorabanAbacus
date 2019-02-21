1279d47a4c47b57978cd47d915450e68de25c4ae
Huseyin Kutlukkaya <huseyinkutlukkaya@gmail.com>
2019-02-21 23:30:42 +0300

    Proje Eklendi

---
+++ Soroban/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Soroban
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tb_Islem = new System.Windows.Forms.TextBox();
+            this.btn_Coz = new System.Windows.Forms.Button();
+            this.btn_ileri = new System.Windows.Forms.Button();
+            this.btn_geri = new System.Windows.Forms.Button();
+            this.btn_ilk = new System.Windows.Forms.Button();
+            this.btn_son = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.Location = new System.Drawing.Point(29, 154);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(579, 287);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tb_Islem
+            // 
+            this.tb_Islem.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tb_Islem.Location = new System.Drawing.Point(29, 34);
+            this.tb_Islem.Name = "tb_Islem";
+            this.tb_Islem.Size = new System.Drawing.Size(640, 53);
+            this.tb_Islem.TabIndex = 1;
+            // 
+            // btn_Coz
+            // 
+            this.btn_Coz.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_Coz.Location = new System.Drawing.Point(701, 37);
+            this.btn_Coz.Name = "btn_Coz";
+            this.btn_Coz.Size = new System.Drawing.Size(102, 50);
+            this.btn_Coz.TabIndex = 2;
+            this.btn_Coz.Text = "ÇÖZ";
+            this.btn_Coz.UseVisualStyleBackColor = true;
+            this.btn_Coz.Click += new System.EventHandler(this.btn_Coz_Click);
+            // 
+            // btn_ileri
+            // 
+            this.btn_ileri.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_ileri.Location = new System.Drawing.Point(857, 206);
+            this.btn_ileri.Name = "btn_ileri";
+            this.btn_ileri.Size = new System.Drawing.Size(141, 64);
+            this.btn_ileri.TabIndex = 3;
+            this.btn_ileri.Text = "İleri";
+            this.btn_ileri.UseVisualStyleBackColor = true;
+            this.btn_ileri.Click += new System.EventHandler(this.btn_ileri_Click);
+            // 
+            // btn_geri
+            // 
+            this.btn_geri.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_geri.Location = new System.Drawing.Point(647, 206);
+            this.btn_geri.Name = "btn_geri";
+            this.btn_geri.Size = new System.Drawing.Size(141, 64);
+            this.btn_geri.TabIndex = 4;
+            this.btn_geri.Text = "Geri";
+            this.btn_geri.UseVisualStyleBackColor = true;
+            this.btn_geri.Click += new System.EventHandler(this.btn_geri_Click);
+            // 
+            // btn_ilk
+            // 
+            this.btn_ilk.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_ilk.Location = new System.Drawing.Point(647, 288);
+            this.btn_ilk.Name = "btn_ilk";
+            this.btn_ilk.Size = new System.Drawing.Size(141, 64);
+            this.btn_ilk.TabIndex = 6;
+            this.btn_ilk.Text = "İLK";
+            this.btn_ilk.UseVisualStyleBackColor = true;
+            this.btn_ilk.Click += new System.EventHandler(this.btn_ilk_Click);
+            // 
+            // btn_son
+            // 
+            this.btn_son.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_son.Location = new System.Drawing.Point(857, 288);
+            this.btn_son.Name = "btn_son";
+            this.btn_son.Size = new System.Drawing.Size(141, 64);
+            this.btn_son.TabIndex = 5;
+            this.btn_son.Text = "SON";
+            this.btn_son.UseVisualStyleBackColor = true;
+            this.btn_son.Click += new System.EventHandler(this.btn_son_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1020, 460);
+            this.Controls.Add(this.btn_ilk);
+            this.Controls.Add(this.btn_son);
+            this.Controls.Add(this.btn_geri);
+            this.Controls.Add(this.btn_ileri);
+            this.Controls.Add(this.btn_Coz);
+            this.Controls.Add(this.tb_Islem);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox tb_Islem;
+        private System.Windows.Forms.Button btn_Coz;
+        private System.Windows.Forms.Button btn_ileri;
+        private System.Windows.Forms.Button btn_geri;
+        private System.Windows.Forms.Button btn_ilk;
+        private System.Windows.Forms.Button btn_son;
+    }
+}
+
---
+++ Soroban/Form1.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soroban
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int[]> Dizi = new List<int[]>();
+        List<string[]> Aciklamalar = new List<string[]>();
+        String TxtIslem;
+        int index;
+        int SayfaNo = 0;
+        void TabloyuCiz()
+        {
+            dataGridView1.Columns.Clear();
+            for (int i = 0; i < 13; i++)//13 tane sutun eklenir
+            {
+                dataGridView1.Columns.Add("","");
+                dataGridView1.Columns[i].Width = 40;
+            }
+            //7 tane satır eklenir 5 tanesi aşadağıdakileri için 2 tanesi yukardakiler için 1 er adet haraket için en yukardaki değerleri en aşağıdaki açıklama
+            for (int i = 0; i < 9; i++)
+            {
+                dataGridView1.Rows.Add();
+            }
+        } 
+        void Gir(int sayi)
+        {
+            int i = 12;
+            while (sayi > 0)
+            {
+
+                Dizi[Dizi.Count - 1][i] = sayi % 10;
+                i--;
+                sayi = sayi / 10;
+            }
+        }
+       void IslemAlgilama()
+        {
+            try
+            {
+
+                int sirano = 0;
+                ArrayList liste = new ArrayList();
+                liste.AddRange(TxtIslem.Split('+', '-', '*'));
+                char[] a = TxtIslem.ToCharArray();
+                sirano = -1;
+                if (TxtIslem.Contains("*"))//Eğer çarpma işlemi varsa
+                {
+                    for (int i = 0; i < liste.Count; i++)
+                    {
+                        if (i != liste.Count - 1)
+                        {
+                            sirano += liste[i].ToString().Length + 1;
+
+                            if (a[sirano] == '*')
+                            {
+                                Carpma(Convert.ToInt32(liste[i]), Convert.ToInt32(liste[i + 1]));
+                                if (TxtIslem.Length == (liste[i] + "*" + liste[i + 1]).Length)
+                                    return;
+                                else
+                                {
+                                    TxtIslem = TxtIslem.Replace((liste[i] + "*" + liste[i + 1]), AbaküstekiDegeriAl(Dizi.Count - 1));
+                                    Dizi.Add(new int[13]);
+                                    Aciklamalar.Add(new string[13]);
+                                    for (int j = 0; j < 13; j++)
+                                        Aciklamalar[Aciklamalar.Count - 1][j] = "";
+                                    break;
+                                }
+
+                            }
+                        }
+
+
+                    }
+                    IslemAlgilama();
+                }
+                else//Çarpma işlemi yoksa
+                {
+
+                    for (int i = 0; i < liste.Count; i++)
+                    {
+                        if (i == 0)
+                            topla(Convert.ToInt32(liste[i]), 0);
+                        else
+                        {
+                            if (a[sirano] == '+')
+                                topla(Convert.ToInt32(liste[i]), 0);
+                            else
+                                Cikar(Convert.ToInt32(liste[i]), 0);
+
+                        }
+                        sirano += liste[i].ToString().Length + 1;
+
+
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Girişinizi Kontrol Ediniz.");
+
+            }
+          
+        }
+        string AbaküstekiDegeriAl(int c)
+        {
+            string a="";
+            bool b = false;
+            for (int i = 0; i < 13; i++)
+            {
+                if (Dizi[c][i] != 0)
+                    b = true;
+                if (b)
+                    a += Dizi[c][i].ToString();
+             
+
+            }
+
+
+           
+            return a;
+        }
+      void topla(int sayi,int bosluk)
+        {
+            try
+            {
+                List<int> sayilar = new List<int>();
+                sayi = sayi * Convert.ToInt32(Math.Pow(10, bosluk));
+                while (sayi > 0)
+                {
+                    sayilar.Add(sayi % 10);
+                    sayi = sayi / 10;
+                }
+
+                for (int i = 0; i < sayilar.Count; i++)
+                {
+
+                    if (sayilar[i] == 0)
+                    {
+                        i++;
+                        if (i == sayilar.Count)
+                            break;
+                    }
+
+                    if (4 - (Dizi[Dizi.Count - 1][12 - i] % 5) >= sayilar[i] && sayilar[i] < 5)
+                    {
+                        Dizi[Dizi.Count - 1][12 - i] += sayilar[i];
+                    }
+                    else
+                    {
+
+                        if (9 - Dizi[Dizi.Count - 1][12 - i] >= sayilar[i])//5li
+                        {
+
+                            Dizi[Dizi.Count - 1][12 - i] += 5;
+
+
+                            Dizi[Dizi.Count - 1][12 - i] -= 5 - sayilar[i];
+                        }
+                        else//10lu
+                        {
+                            Cikar(10 - sayilar[i], i);
+                            topla(1, i + 1);
+
+
+
+                        }
+
+                    }
+                    if (AbaküstekiDegeriAl(Dizi.Count - 1) != AbaküstekiDegeriAl(Dizi.Count - 2))
+                        YeniAdim();
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Girişinizi Kontrol Ediniz.");
+            }
+           
+        }
+        void Cikar(int sayi, int bosluk)
+        {
+            try
+            {
+                List<int> sayilar = new List<int>();
+                sayi = sayi * Convert.ToInt32(Math.Pow(10, bosluk));
+                while (sayi > 0)
+                {
+                    sayilar.Add(sayi % 10);
+                    sayi = sayi / 10;
+                }
+                for (int i = 0; i < sayilar.Count; i++)
+                {
+                    if (sayilar[i] == 0)
+                    {
+                        i++;
+                        if (i == sayilar.Count)
+                            break;
+                    }
+                    if ((Dizi[Dizi.Count - 1][12 - i] % 5) >= sayilar[i])
+                    {
+                        Dizi[Dizi.Count - 1][12 - i] -= sayilar[i];
+                    }
+                    else
+                    {
+                        if (Dizi[Dizi.Count - 1][12 - i] >= sayilar[i])//5li
+                        {
+
+
+                            Dizi[Dizi.Count - 1][12 - i] -= 5;
+
+                            Dizi[Dizi.Count - 1][12 - i] += 5 - sayilar[i];
+                        }
+                        else//10lu
+                        {
+                            Cikar(1, i + 1);
+                            topla(10 - sayilar[i], i);
+
+
+                        }
+
+                    }
+                    if (AbaküstekiDegeriAl(Dizi.Count - 1) != AbaküstekiDegeriAl(Dizi.Count - 2))
+                        YeniAdim();
+
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Girişinizi Kontrol Ediniz.");
+            }
+           
+        }
+        void Carpma(int sayi,int sayi2)
+        {
+
+            List<int> sayilar = new List<int>();
+            List<int> sayilar2 = new List<int>();
+
+            int t = sayi;
+            while (t > 0)
+            {
+
+
+                sayilar.Add(t % 10);
+                t = t / 10;
+
+            }
+
+            t = sayi2;
+            while (t > 0)
+            {
+
+
+                sayilar2.Add(t % 10);
+                t = t / 10;
+
+            }
+
+            for (int i = 0; i < GetBasamakSayisi(sayi2); i++)
+            {
+                for (int j =0; j < GetBasamakSayisi(sayi); j++ )
+                {
+                    topla(sayilar[j] * sayilar2[i],i+j);
+
+                }
+               
+               
+            }
+
+        }
+        int GetBasamakSayisi(int i)
+        {
+            int a = 0;
+            while (i > 0)
+            {
+
+
+
+                i = i / 10;
+                a++;
+
+            }
+            return a;
+
+        }
+        void AbakusuCiz(int dizino)
+        {
+
+            TabloyuCiz();
+            for (int i = 0; i < 13; i++)
+            {
+                dataGridView1.Rows[0].Cells[i].Value = Dizi[dizino][i].ToString();
+                dataGridView1.Rows[8].Cells[i].Value = Aciklamalar[dizino][i].ToString();
+
+
+            }
+
+            for (int i = 0; i < 13; i++)
+            {
+           
+                if (Dizi[dizino][i]>=5)
+                {
+                    dataGridView1.Rows[2].Cells[i].Style.BackColor = Color.Black;
+                }
+                else
+                {
+                    dataGridView1.Rows[1].Cells[i].Style.BackColor = Color.Black;
+
+                }
+                for (int a = 0; a < Dizi[dizino][i]%5; a++)
+                {
+
+                    dataGridView1.Rows[a + 3].Cells[i].Style.BackColor = Color.Brown;
+
+                }
+                for (int a = 0; a < 4-(Dizi[dizino][i] % 5); a++)
+                {
+                    dataGridView1.Rows[7 - a].Cells[i].Style.BackColor = Color.Brown;
+
+                }
+              
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+           
+
+         
+            TabloyuCiz();
+        }
+        void YeniAdim()
+        {
+            Dizi.Add(new int[13]);
+            Aciklamalar.Add(new string[13]);
+            for (int i = 0; i < 13 ; i++)
+                Aciklamalar[Aciklamalar.Count - 1][i] = "";
+                for (int i = 0; i < 13&&Dizi.Count>1; i++)
+                Dizi[Dizi.Count - 1][i] = Dizi[Dizi.Count - 2][i];
+            
+        }
+        private void btn_Coz_Click(object sender, EventArgs e)
+        {
+           
+            Dizi.Clear();
+            Aciklamalar.Clear();
+            SayfaNo = 0;
+            YeniAdim();
+            YeniAdim();
+
+            TxtIslem = tb_Islem.Text;
+         
+            IslemAlgilama();
+            AbakusuCiz(0);
+
+
+        }
+
+        private void btn_ileri_Click(object sender, EventArgs e)
+        {
+            if (SayfaNo < Dizi.Count-1)
+            {
+                SayfaNo++;
+
+                AbakusuCiz(SayfaNo);
+            }
+        }
+
+        private void btn_geri_Click(object sender, EventArgs e)
+        {
+            if (SayfaNo >= 1)
+            {
+                SayfaNo--;
+
+                AbakusuCiz(SayfaNo);
+            }
+        }
+
+     
+
+        private void btn_son_Click(object sender, EventArgs e)
+        {
+            SayfaNo = Dizi.Count - 1;
+            AbakusuCiz(SayfaNo);
+
+        }
+
+        private void btn_ilk_Click(object sender, EventArgs e)
+        {
+
+            SayfaNo = 0;
+            AbakusuCiz(SayfaNo);
+        }
+    }
+}
